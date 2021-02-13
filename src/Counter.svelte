e5d2a79fcd847b2ac61d7fd2e61886b33c9059cd
Alvaro Felipe García Méndez <alvarofelipe12@gmail.com>
2021-02-13 18:45:05 -0500

    Component and state
    - Create a new component.
    - Handle states and props.

---
+++ src/Counter.svelte
@@ -0,0 +1,48 @@
+<script>
+	export let name;
+    export let initialCounter;
+    export let counter = 0;
+    counter = initialCounter;
+    
+    function handleClick() {
+        name = 'other name';
+    }
+
+    function counterHandleClick() {
+        counter++;
+    }
+</script>
+
+<main>
+    <button on:click="{handleClick}">Change name</button>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <button on:click="{counterHandleClick}">Increment</button>
+    <span>{counter}</span>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+    span {
+        color: red;
+    }
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
