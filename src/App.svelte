fc760700df84054a5b1219a9b7d4965aef8aea90
Alvaro Felipe García Méndez <alvarofelipe12@gmail.com>
2021-02-16 17:49:00 -0500

    Conditional rendering
    - Add conditional rendering to show a loader while fecthing.
    - Add conditional rendering to show how many movies where fetched.

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <script>
-    import Counter from "./Counter.svelte";
+    // import Counter from "./Counter.svelte";
     import Input from "./Input.svelte"
 
-    export let initialCounter;
-    export let name;
+    // export let initialCounter;
+    // export let name;
     const commonProps = { maxCounter: 5 };
 </script>
 
-<Counter initialCounter={initialCounter} name={name} {... commonProps} />
-<Counter {... commonProps} />
+<!-- <Counter initialCounter={initialCounter} name={name} {... commonProps} />
+<Counter {... commonProps} /> -->
 <Input />
--- src/Input.svelte
+++ src/Input.svelte
@@ -1,16 +1,31 @@
 <script>
     let value = "";
     let response = [];
+    let loading = false;
     const handleInput = (event) => (value = event.target.value);
 
     $: if (value.length > 2) {
+        loading = true;
         fetch(`https://www.omdbapi.com/?s=${value}&apikey=daa402f6`)
-        .then(res => res.json())
-        .then(apiResponse => {
-            response = apiResponse.Search;
-            console.log(apiResponse);
-        });
+            .then((res) => res.json())
+            .then((apiResponse) => {
+                response = apiResponse.Search || [];
+                console.log(apiResponse);
+                loading = false;
+            });
     }
 </script>
 
-<input type="text" {value} on:input={handleInput} />
+<input
+    type="text"
+    {value}
+    on:input={handleInput}
+    placeholder="Search movies..."
+/>
+{#if loading}
+    <strong>Loading...</strong>
+{:else if response.length > 0}
+    <strong>We have {response.length} movies</strong>
+{:else}
+    <strong>We don't have movies</strong>
+{/if}
