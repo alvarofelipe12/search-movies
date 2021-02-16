aa7f41a3b7876e6a5b1d1778b1344685c619e6f0
Alvaro Felipe García Méndez <alvarofelipe12@gmail.com>
2021-02-16 17:22:19 -0500

    Fetch and reactive declaration
    - Add Component input.
    - Fetch api data.
    - Add vscode debug browser.

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,12 @@
 <script>
-    import Counter from './Counter.svelte'
+    import Counter from "./Counter.svelte";
+    import Input from "./Input.svelte"
+
     export let initialCounter;
     export let name;
+    const commonProps = { maxCounter: 5 };
 </script>
 
-<Counter initialCounter={initialCounter} name={name}/>
-<Counter />
+<Counter initialCounter={initialCounter} name={name} {... commonProps} />
+<Counter {... commonProps} />
+<Input />
---
+++ src/Input.svelte
@@ -0,0 +1,16 @@
+<script>
+    let value = "";
+    let response = [];
+    const handleInput = (event) => (value = event.target.value);
+
+    $: if (value.length > 2) {
+        fetch(`https://www.omdbapi.com/?s=${value}&apikey=daa402f6`)
+        .then(res => res.json())
+        .then(apiResponse => {
+            response = apiResponse.Search;
+            console.log(apiResponse);
+        });
+    }
+</script>
+
+<input type="text" {value} on:input={handleInput} />
